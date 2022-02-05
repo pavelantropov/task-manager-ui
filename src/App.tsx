291298dc7ba7bb3bbbd54c86ef22b7bf67079c9f
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-05 18:39:36 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./features/layout/Layout";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./features/layout/Header";
-import Footer from "./features/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </>
   );
 }
 
