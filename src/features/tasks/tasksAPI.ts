535de6c6d7c5caca8e9802a6cf2a605fa2515cb5
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-08 01:31:47 +0400

    Remove the getTask method, use getTasks instead

--- src/features/tasks/tasksAPI.ts
+++ src/features/tasks/tasksAPI.ts
@@ -1,22 +1,16 @@
+import { DefaultTasks } from "../../data/data";
 import Task from "./types";
 
-export function getTask(taskId: number): Promise<Task> {
-  const task: Task = {
-    title: "Test task",
-    description: "A task to test if it works",
-    deadline: new Date(),
-    labels: ["label1", "label2"],
-  };
-
-  if (process.env.NODE_ENV === "test") {
-    return new Promise<Task>((resolve) =>
-      setTimeout(() => resolve(task), 1500)
+export function getTasks(): Promise<Task[]> {
+  if (process.env.NODE_ENV != "test") {
+    return new Promise<Task[]>((resolve) =>
+      setTimeout(() => resolve(DefaultTasks), 1500)
     );
   } else {
-    return fetch(`${process.env.REACT_APP_API_URI}/api/tasks/${taskId}`, {
+    return fetch(`${process.env.REACT_APP_API_URI}/api/tasks`, {
       method: "GET",
     })
       .then((response) => response.json)
-      .then((data) => data as unknown as Promise<Task>);
+      .then((data) => data as unknown as Promise<Task[]>);
   }
 }
