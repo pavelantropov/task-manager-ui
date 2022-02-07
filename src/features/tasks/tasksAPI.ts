40936799282cb1964cef91f4354de3e660a660cb
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-08 00:11:19 +0400

    Add an API call to getTask()

--- src/features/tasks/tasksAPI.ts
+++ src/features/tasks/tasksAPI.ts
@@ -1,14 +1,22 @@
-import Task from "./task";
+import Task from "./types";
 
-export function getTask(): Promise<Task> {
-  const detail: Task = {
+export function getTask(taskId: number): Promise<Task> {
+  const task: Task = {
     title: "Test task",
-    description: "A task to test, if it works",
+    description: "A task to test if it works",
     deadline: new Date(),
     labels: ["label1", "label2"],
   };
 
-  return new Promise<Task>((resolve) =>
-    setTimeout(() => resolve(detail), 1500)
-  );
+  if (process.env.NODE_ENV === "test") {
+    return new Promise<Task>((resolve) =>
+      setTimeout(() => resolve(task), 1500)
+    );
+  } else {
+    return fetch(`${process.env.REACT_APP_API_URI}/api/tasks/${taskId}`, {
+      method: "GET",
+    })
+      .then((response) => response.json)
+      .then((data) => data as unknown as Promise<Task>);
+  }
 }
