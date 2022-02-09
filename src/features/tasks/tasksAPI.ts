7229e230a87d40135a215d9b1b6c7d14661e6c3b
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-09 18:58:31 +0400

    Use async methods for API calls

--- src/features/tasks/tasksAPI.ts
+++ src/features/tasks/tasksAPI.ts
@@ -1,17 +1,19 @@
 import { DefaultTasks } from "../../data/data";
 import Task, { CreateTaskRequest, CreateTaskResponse } from "./types";
 
-export function getTasks(): Promise<Task[]> {
+export async function getTasks(): Promise<Task[]> {
   if (process.env.NODE_ENV != "test") {
     return new Promise<Task[]>((resolve) =>
       setTimeout(() => resolve(DefaultTasks), 1500)
     );
   } else {
-    return fetch(`${process.env.REACT_APP_API_URI}/api/tasks`, {
+    return await fetch(`${process.env.REACT_APP_API_URI}/api/tasks`, {
       method: "GET",
     })
       .then((response) => response.json)
       .then((data) => data as unknown as Promise<Task[]>);
+  }
+}
 
 export async function createTask(
   params: CreateTaskRequest
