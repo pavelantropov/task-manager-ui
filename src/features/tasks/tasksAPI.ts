40647f69e7abcf55910bd1466ca047d34de5ca2b
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-11 19:32:31 +0400

    Update the api get call

--- src/features/tasks/tasksAPI.ts
+++ src/features/tasks/tasksAPI.ts
@@ -1,18 +1,27 @@
 import { DefaultTasksArray } from "../../data/data";
-import Task, { CreateTaskRequest, CreateTaskResponse } from "./types";
+import Task, {
+  GetTasksResponse,
+  CreateTaskRequest,
+  CreateTaskResponse,
+} from "./types";
 
-export async function getTasks(): Promise<Task[]> {
-  if (process.env.NODE_ENV === "test") {
-    return new Promise<Task[]>((resolve) =>
-      setTimeout(() => resolve(DefaultTasksArray), 1500)
-    );
-  } else {
-    return (await fetch(`${process.env.REACT_APP_API_URI}/api/tasks`, {
-      method: "GET",
-      headers: { Accept: "application/json" },
-    })
-      .then((response) => response.json())) as unknown as Task[];
-  }
+export function getTasks(): Promise<GetTasksResponse> {
+  return new Promise<GetTasksResponse>((resolve) => {
+    let data: Task[];
+
+    if (process.env.NODE_ENV === "test") {
+      data = DefaultTasksArray;
+    } else {
+      data = fetch(`${process.env.REACT_APP_API_URI}/api/tasks`, {
+        method: "GET",
+        headers: { Accept: "application/json" },
+      }).then((response) => response.json()) as unknown as Task[];
+    }
+
+    resolve({
+      data: data,
+    } as GetTasksResponse);
+  });
 }
 
 export async function createTask(
