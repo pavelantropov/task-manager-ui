c12a37d65e1441046faa2d6c8874f34736fef8f2
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-12 01:01:11 +0400

    Clean the page layout

--- src/features/layout/Layout.tsx
+++ src/features/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import logo from "../../assets/reactLogo.svg";
 import Header from "./Header";
 import Footer from "./Footer";
 import Project from "../tasks/Project";
@@ -12,20 +11,6 @@ const Layout = () => {
       <Container>
         <Project />
       </Container>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Footer />
     </>
   );
